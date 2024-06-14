ff2f68ff1a07d986a5bdc79a1ca3abe6e1c22e8c
justaprogrammer321 <architbhrgv@gmail.com>
2024-06-14 23:34:00 +0530

    used the socket.emit correctly in the client side

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
 const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const io = socketIo(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    io.emit('userconnect',`user connected-${socket.id}`)
-    socket.on('disconnect', () => {
-      io.emit('userdisconnect','user disconnected')
-    });
+  console.log('A user connected');
+
+  socket.on('join', (nickname) => {
+    socket.nickname = nickname;
+    io.emit('user connected', `${nickname} has connected`);
   });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message',msg)
+  socket.on('message', (msg) => {
+    io.emit('message', { nickname: socket.nickname, message: msg });
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.nickname) {
+      io.emit('user disconnected', `${socket.nickname} has disconnected`);
+    }
+    console.log('A user disconnected');
   });
 });
 
 server.listen(3000, () => {
-  console.log('listening on 3000');
+  console.log('Server is running on port 3000');
 });
